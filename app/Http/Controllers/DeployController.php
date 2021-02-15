b2c7035df7f08eaf002ee901c7ced8f1b956109c
Gastón Nan <nangastonr@gmail.com>
2021-02-15 17:30:58 -0300

    deploy controller fix

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class DeployController extends Controller
@@ -14,11 +15,11 @@ class DeployController extends Controller
 		$localToken = config('app.deploy_secret');
 		$localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
 		if (hash_equals($githubHash, $localHash)) {
+			echo "sha ok!"
 			$root_path = base_path();
 			$process = new Process(["cd {$root_path}", './deploy.sh']);
-			$process->run(function ($type, $buffer) {
-				echo $buffer;
-			});
+			echo $process->isSuccessful() ? "Exito!" : "Algo salió mal";
+			echo $process->getOutput();
 		}
 	}
 }

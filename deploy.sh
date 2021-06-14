da6975a3dda9edb9ae5e14b1da0ab19721c70912
Gastón Nan <nangastonr@gmail.com>
2021-06-14 20:22:41 -0300

    ajuste en deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 # activate maintenance mode
-php artisan down
+php7.4 artisan down
 # update source code
 git pull
 # update PHP dependencies
-composer install --no-interaction --no-dev --prefer-dist
+php7.4 /usr/local/bin/composer install --no-interaction --no-dev --prefer-dist
 # --no-interaction Do not ask any interactive question
 # --no-dev  Disables installation of require-dev packages.
 # --prefer-dist  Forces installation from package dist even for dev versions.
 # update database
-php artisan migrate --force
+php7.4 artisan migrate --force
 # --force  Required to run when in production.
 
 #clear cache
-php artisan route:cache
-php artisan config:cache
-php artisan view:cache
+php7.4 artisan route:cache
+php7.4 artisan config:cache
+php7.4 artisan view:cache
 
 # stop maintenance mode
-php artisan up
+php7.4 artisan up
